65513b2e55d72c866dbe6af3129892fb7a4d010e
Ben Williams <benj@minwilliams.net>
2014-11-20 13:49:55 +1100

    Removed svg that was causing font to be off center

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,6 @@ gulp.task('build--fonts', function(cb) {
       fontName:         FONT_NAME,
       appendCodepoints: false,
       normalize:        true,
-      descent:          -12,  //some magic number to make the font offset==0
       log:              false //replace with `function() {}` to disable logging
     }))
     .on('codepoints', function(codepoints, options) {
